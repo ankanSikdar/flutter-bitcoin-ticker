def445b2b7899b1c51dae00964806e9dda958db6
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-15 18:02:30 +0530

    Finished All 3 Challenges

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,15 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  final apiKey = '?apikey=C27C770CB08D4B9D8333A22B3993F47A';
+  final coinApi = 'https://rest.coinapi.io/v1/exchangerate/';
+
+  Future<Map> getRate(String first, String second) async {
+    http.Response response = await http.get('$coinApi$first/$second$apiKey');
+    if (response.statusCode == 200) {
+      Map data = convert.jsonDecode(response.body);
+      return data;
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -10,6 +10,9 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String currentCurrency = 'USD';
+  int rate1;
+  int rate2;
+  int rate3;
 
   DropdownButton androidDropDown() {
     List<DropdownMenuItem<String>> dropDownItems = [];
@@ -26,6 +29,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           currentCurrency = value;
+          getData();
         });
       },
     );
@@ -41,7 +45,10 @@ class _PriceScreenState extends State<PriceScreen> {
       backgroundColor: Colors.lightBlue,
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
-        print(selectedIndex);
+        setState(() {
+          currentCurrency = currenciesList[selectedIndex];
+          getData();
+        });
       },
       children: items,
     );
@@ -53,6 +60,24 @@ class _PriceScreenState extends State<PriceScreen> {
     } else if (Platform.isAndroid) {
       return androidDropDown();
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    CoinData coinData = CoinData();
+    Map data1 = await coinData.getRate(cryptoList[0], currentCurrency);
+    Map data2 = await coinData.getRate(cryptoList[1], currentCurrency);
+    Map data3 = await coinData.getRate(cryptoList[2], currentCurrency);
+    setState(() {
+      rate1 = data1['rate'].toInt();
+      rate2 = data2['rate'].toInt();
+      rate3 = data3['rate'].toInt();
+    });
   }
 
   @override
@@ -76,7 +101,55 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  rate1 != null
+                      ? '1 ${cryptoList[0]} = $rate1 $currentCurrency'
+                      : '1 ${cryptoList[0]} = ? $currentCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  rate2 != null
+                      ? '1 ${cryptoList[1]} = $rate2 $currentCurrency'
+                      : '1 ${cryptoList[1]} = ? $currentCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  rate3 != null
+                      ? '1 ${cryptoList[2]} = $rate3 $currentCurrency'
+                      : '1 ${cryptoList[2]} = ? $currentCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -91,10 +164,12 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS ? getIOSPicker() : androidDropDown(),
+            child: getIOSPicker(),
           )
         ],
       ),
     );
   }
 }
+
+//Platform.isIOS ? getIOSPicker() : androidDropDown()
